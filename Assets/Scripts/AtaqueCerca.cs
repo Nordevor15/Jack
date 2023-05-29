c93cf1a882b911293bcafba6c18c7af32c090fb9
Nordevor15 <elefteriualexis@gmail.com>
2023-05-29 02:20:35 -0500

    Alexis

--- Assets/Scripts/AtaqueCerca.cs
+++ Assets/Scripts/AtaqueCerca.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class AtaqueCerca : MonoBehaviour
 {
-    public float attackDistance = 1f; // Distancia a la que el jugador puede atacar
-    public string enemyTag = "Enemy1"; // Etiqueta asignada a los enemigos
-    public int attackDamage = 1; // Daño que hace el jugador al atacar
+    public float attackDistance = 1f; 
+    public string enemyTag = "Enemy1"; 
+    public int attackDamage = 1;
 
     void Update()
     {
-        // Detectar si el jugador presionó la barra espaciadora
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (Input.GetKeyDown(GetButtonDown("Fire1"))
         {
-            // Detectar si hay un enemigo cerca del jugador
             Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, attackDistance);
 
-            // Si hay un enemigo cerca, reducir su vida
             foreach (Collider2D enemy in hitEnemies)
             {
                 if (enemy.CompareTag(enemyTag))
@@ -27,7 +25,6 @@ public class AtaqueCerca : MonoBehaviour
         }
     }
 
-    // Dibujar un círculo en la escena para visualizar el rango de ataque del jugador
     void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(transform.position, attackDistance);
